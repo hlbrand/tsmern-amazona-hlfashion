b6d9e9e37fa7d22bbaac1f8125f0a4d676605146
hlbrand2995@gmail.com <hlbrand2995@gmail.com>
2023-03-24 18:04:25 +0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <header>HF fashion</header>
+      <main>asfd</main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
